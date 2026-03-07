82ea9aea7dca3b2a7b1dbf06e02cb1b38441c4b1
LightningBolt333 <miguelduarteesgb@gmail.com>
2026-03-07 13:27:03 +0000

    ex 7 done. App now lets user draw and save drawing to gallery

--- app/src/main/java/dam_a51446/helloworld/MainActivity.kt
+++ app/src/main/java/dam_a51446/helloworld/MainActivity.kt
@@ -1,10 +1,18 @@
 package dam_a51446.helloworld
 
+import android.content.ContentValues
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,5 +26,17 @@ class MainActivity : AppCompatActivity() {
         }
         println(this@MainActivity.localClassName + getString(R.string.activity_oncreate_msg))
         println(getString(R.string.activity_oncreate_msg, this@MainActivity.localClassName))
+
+        val drawingView = findViewById<DrawingView>(R.id.drawingView)
+        val saveButton = findViewById<Button>(R.id.saveButton)
+
+        saveButton.setOnClickListener {
+            val bitmap = drawingView.getBitmap()
+
+            MediaStore.Images.Media.insertImage(contentResolver, bitmap, "Drawing", "My sketch")
+            Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show()
+
+            drawingView.clearCanvas()
+        }
     }
 }
