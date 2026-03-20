ff683dcd9ece6d35a13bf951b104181154e36464
LightningBolt333 <miguelduarteesgb@gmail.com>
2026-03-20 16:33:57 +0000

    filterByUser done, changes to sealed class

--- src/main/kotlin/dam/Event.kt
+++ src/main/kotlin/dam/Event.kt
@@ -3,7 +3,17 @@ package dam
 sealed class Event {
     //Classes sealed são usadas para representar uma hierarquia de classes restrita. Netse caso é ideal porque um evento só pode ser destes 3 tipos
     //é como se fosse um enum, mas cada constante pode ser uma classe que tem a sua própria informação
-    data class Login(val username: String, val timestamp: Long)
-    data class Purchase(val username: String, val amount: Double, val timestamp: Long)
-    data class Logout(val username: String, val timestamp: Long)
+    data class Login(val username: String, val timestamp: Long) : Event()
+    data class Purchase(val username: String, val amount: Double, val timestamp: Long) : Event()
+    data class Logout(val username: String, val timestamp: Long) : Event()
+}
+
+fun List<Event>.filterByUser(username: String): List<Event> {
+    return this.filter {
+        when (it) {
+            is Event.Login -> it.username == username
+            is Event.Purchase -> it.username == username
+            is Event.Logout -> it.username == username
+        }
+    }
 }
