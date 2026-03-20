dfb9e3b7fc9b1301faddd5405008651c05959e5f
LightningBolt333 <miguelduarteesgb@gmail.com>
2026-03-20 16:22:41 +0000

    sealed class commented

--- src/main/kotlin/dam/Event.kt
+++ src/main/kotlin/dam/Event.kt
@@ -1,6 +1,8 @@
 package dam
 
 sealed class Event {
+    //Classes sealed são usadas para representar uma hierarquia de classes restrita. Netse caso é ideal porque um evento só pode ser destes 3 tipos
+    //é como se fosse um enum, mas cada constante pode ser uma classe que tem a sua própria informação
     data class Login(val username: String, val timestamp: Long)
     data class Purchase(val username: String, val amount: Double, val timestamp: Long)
     data class Logout(val username: String, val timestamp: Long)
