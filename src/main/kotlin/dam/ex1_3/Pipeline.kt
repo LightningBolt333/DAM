036e8335c4c146b1c7c027f04307784f41ff9545
LightningBolt333 <miguelduarteesgb@gmail.com>
2026-03-21 23:25:12 +0000

    changed "steps" to "stages". created execute method

--- src/main/kotlin/dam/ex1_3/Pipeline.kt
+++ src/main/kotlin/dam/ex1_3/Pipeline.kt
@@ -1,10 +1,19 @@
 package dam.ex1_3
 
 class Pipeline {
-    private val steps = mutableListOf<Pair<String, (List<String>) -> List<String>>>()
+    private val stages = mutableListOf<Pair<String, (List<String>) -> List<String>>>()
 
-    fun addStage(name: String, transform: (List<String>) -> List<String>){
-        steps.add(name to transform)
+    fun addStage(name: String, transform: (List<String>) -> List<String>) {
+        stages.add(name to transform)
         //keyword "to" cria um tuplo de name e transform, ou seja o setep
+    }
+
+    fun execute(input: List<String>): List<String> {
+        var currentResult = input
+
+        for((_, transform) in stages){
+            currentResult = transform(currentResult)
+        }
+        return currentResult
     }
 }
