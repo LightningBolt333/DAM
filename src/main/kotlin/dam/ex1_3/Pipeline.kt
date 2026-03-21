8d750419dcf34f12f41a61afdc9f84bd099e8937
LightningBolt333 <miguelduarteesgb@gmail.com>
2026-03-21 23:22:45 +0000

    created addStage method

--- src/main/kotlin/dam/ex1_3/Pipeline.kt
+++ src/main/kotlin/dam/ex1_3/Pipeline.kt
@@ -3,4 +3,8 @@ package dam.ex1_3
 class Pipeline {
     private val steps = mutableListOf<Pair<String, (List<String>) -> List<String>>>()
 
+    fun addStage(name: String, transform: (List<String>) -> List<String>){
+        steps.add(name to transform)
+        //keyword "to" cria um tuplo de name e transform, ou seja o setep
+    }
 }
