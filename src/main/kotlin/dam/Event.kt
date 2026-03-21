788a4fb53f2386e6652388eb8df4e5500b33416e
LightningBolt333 <miguelduarteesgb@gmail.com>
2026-03-21 21:07:08 +0000

    ex 1.1 complete

--- src/main/kotlin/dam/Event.kt
+++ src/main/kotlin/dam/Event.kt
@@ -28,6 +28,34 @@ fun List<Event>.totalSpent(username: String): Double {
 //higher-order functions são funções que usam outras funções como parâmetros
 //também podem retornar uma função como resultado
 //neste caso usa umafunção que pega num objeto Event e retorna nada (unit no kotlin é equivalente a void no java, mas é um objeto em vez de ser nada)
-fun processEvent(events: List<Event>, handler: (Event) -> Unit) {
+fun processEvents(events: List<Event>, handler: (Event) -> Unit) {
     events.forEach {handler(it)}
+}
+
+fun main() {
+
+    val events = listOf (
+        Event.Login ("alice", 1000),
+        Event.Purchase ("alice", 49.99, 1100),
+        Event.Purchase ("bob", 19.99, 1200),
+        Event.Login ("bob", 1050),
+        Event.Purchase ("alice", 15.00, 1300),
+        Event.Logout ("alice", 1400),
+        Event.Logout ("bob", 1500)
+    )
+
+    processEvents(events) { event ->
+        when (event) {
+            is Event.Login -> println("[LOGIN] ${event.username} logged in at t=${event.timestamp}")
+            is Event.Purchase -> println("[PURCHASE] ${event.username} spent $${event.amount} at t=${event.timestamp}")
+            is Event.Logout -> println("[LOGOUT] ${event.username} logged out at t=${event.timestamp}")
+
+        }
+    }
+
+    println("Total spent by Alice: $${events.totalSpent("alice")}")
+    println("Total spent by Bob: $${events.totalSpent("bob")}")
+    println("Events for Alice: ")
+    events.filterByUser("alice").forEach {println(it)}
+
 }
