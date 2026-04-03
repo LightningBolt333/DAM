93387987e86bef1e8d97eec1372090fd5b0c4de1
LightningBolt333 <miguelduarteesgb@gmail.com>
2026-04-03 19:19:25 +0100

    overloaded arithmetic operators

--- src/main/kotlin/dam/ex1_4/Vec2.kt
+++ src/main/kotlin/dam/ex1_4/Vec2.kt
@@ -2,4 +2,8 @@ package dam.ex1_4
 
 
 data class Vec2(val x: Double, val y: Double) {
+    operator fun plus(other: Vec2) = Vec2(x + other.x, y + other.y)
+    operator fun minus(other: Vec2) = Vec2(x - other.x, y - other.y)
+    operator fun times(scalar: Double) = Vec2(x * scalar, y * scalar)
+    operator fun unaryMinus() = Vec2(-x, -y)
 }
