e1abef1daefacd707448ab051dc6083b6dd2cb2c
LightningBolt333 <miguelduarteesgb@gmail.com>
2026-04-03 19:29:21 +0100

    overloaded compareTo to compare magnitudes

--- src/main/kotlin/dam/ex1_4/Vec2.kt
+++ src/main/kotlin/dam/ex1_4/Vec2.kt
@@ -1,9 +1,15 @@
 package dam.ex1_4
-
+import kotlin.math.sqrt
 
 data class Vec2(val x: Double, val y: Double) {
     operator fun plus(other: Vec2) = Vec2(x + other.x, y + other.y)
     operator fun minus(other: Vec2) = Vec2(x - other.x, y - other.y)
     operator fun times(scalar: Double) = Vec2(x * scalar, y * scalar)
     operator fun unaryMinus() = Vec2(-x, -y)
+
+    fun magnitude(): Double = sqrt(x * x + y * y)
+
+    operator fun compareTo(other: Vec2): Int {
+        return this.magnitude().compareTo(other.magnitude())
+    }
 }
